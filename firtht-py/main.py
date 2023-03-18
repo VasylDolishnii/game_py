ca0196f398fa70f13b845d912a0b76a5b6f17042
Vasyl Dolishnii <dolishniivasyl@gmail.com>
2023-03-18 14:21:02 +0100

    created bonus

--- firtht-py/main.py
+++ firtht-py/main.py
@@ -1,7 +1,10 @@
+import random
 import pygame
 from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
+
+FPS = pygame.time.Clock()
 
 screen = width, heigth = 800 ,600
 
@@ -9,26 +12,78 @@ BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 BLUE = 0, 87, 184
 YELLOW = 254, 221, 0
+RED = 255, 0, 0
 
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
 ball.fill(WHITE)
 ball_rect = ball.get_rect()
-ball_speed = 1
+ball_speed = 5
+
+def create_enemy():
+    enemy = pygame.Surface((20, 20))
+    enemy.fill(RED)
+    enemy_rect = pygame.Rect(width, random.randint(0, heigth), *enemy.get_size())
+    enemy_speed = random.randint(2, 5)
+    return [enemy, enemy_rect, enemy_speed]
+
+def create_bonus():
+    bonus = pygame.Surface((20, 20))
+    bonus.fill(YELLOW)
+    bonus_rect = pygame.Rect(heigth, random.randint(0, width), *bonus.get_size())
+    bonus_speed = random.randint(2, 5)
+    return [bonus, bonus_rect, bonus_speed]  
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+CREATE_BONUS = pygame.USEREVENT + 2
+pygame.time.set_timer(CREATE_BONUS, 3000)
+
+enemies = []
+bonuses = []
 
 is_working = True
 
 while is_working: 
+
+    FPS.tick(60)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             is_working = False
+
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+
+        if event.type == CREATE_BONUS:
+            bonuses.append(create_bonus())
 
     pressed_keys = pygame.key.get_pressed()
 
     main_surface.fill(BLACK)
 
     main_surface.blit(ball, ball_rect)
+
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(-enemy[2], 0)
+        main_surface.blit(enemy[0], enemy[1])
+
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+        if ball_rect.colliderect(enemy[1]):
+            enemies.pop(enemies.index(enemy))
+
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(0, bonus[2])
+        main_surface.blit(bonus[0], bonus[1])
+
+        if bonus[1].bottom > heigth:
+            bonuses.pop(bonuses.index(bonus))
+
+        if ball_rect.colliderect(bonus[1]):
+            bonuses.pop(bonuses.index(bonus))
 
     if pressed_keys[K_DOWN] and not ball_rect.bottom >= heigth:
         ball_rect = ball_rect.move(0, ball_speed)
