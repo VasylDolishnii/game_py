b9b987df0c1dc029f84a6d6e183f01cbaf14dd47
Vasyl Dolishnii <dolishniivasyl@gmail.com>
2023-03-18 10:50:07 +0100

    do borders on field for the game

--- firtht-py/main.py
+++ firtht-py/main.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame. constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -15,7 +15,7 @@ main_surface = pygame.display.set_mode(screen)
 ball = pygame.Surface((20, 20))
 ball.fill(WHITE)
 ball_rect = ball.get_rect()
-ball_speed = [1, 1]
+ball_speed = 1
 
 is_working = True
 
@@ -24,17 +24,22 @@ while is_working:
         if event.type == QUIT:
             is_working = False
 
-    ball_rect = ball_rect.move(ball_speed)
-
-    if ball_rect.bottom >= heigth or ball_rect.top <=0:
-        ball_speed[1] = -ball_speed[1]
-        ball.fill(BLUE)
-
-    if ball_rect.right >= width or ball_rect.left <=0:
-        ball_speed[0] = -ball_speed[0]
-        ball.fill(YELLOW)
+    pressed_keys = pygame.key.get_pressed()
 
     main_surface.fill(BLACK)
 
     main_surface.blit(ball, ball_rect)
+
+    if pressed_keys[K_DOWN] and not ball_rect.bottom >= heigth:
+        ball_rect = ball_rect.move(0, ball_speed)
+
+    if pressed_keys[K_UP] and not ball_rect.top < 0:
+        ball_rect = ball_rect.move(0, -ball_speed)
+
+    if pressed_keys[K_RIGHT] and not ball_rect.right >= width:
+        ball_rect = ball_rect.move(ball_speed, 0)
+
+    if pressed_keys[K_LEFT] and not ball_rect.left < 0:
+        ball_rect = ball_rect.move(-ball_speed, 0)
+
     pygame.display.flip()
