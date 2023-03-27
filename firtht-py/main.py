55febdaa47358d4d3b8e045b24b4538631cecc79
Vasyl Dolishnii <dolishniivasyl@gmail.com>
2023-03-27 18:02:03 +0200

    add png

--- firtht-py/main.py
+++ firtht-py/main.py
@@ -6,7 +6,7 @@ pygame.init()
 
 FPS = pygame.time.Clock()
 
-screen = width, heigth = 800 ,600
+screen = width, heigth = 1200 ,700
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
@@ -14,31 +14,43 @@ BLUE = 0, 87, 184
 YELLOW = 254, 221, 0
 RED = 255, 0, 0
 
+font = pygame.font.SysFont('Verdana', 20)
+
 main_surface = pygame.display.set_mode(screen)
 
-ball = pygame.Surface((20, 20))
-ball.fill(WHITE)
-ball_rect = ball.get_rect()
-ball_speed = 5
+# ball = pygame.Surface((20, 20))
+# ball.fill(WHITE)
+player = pygame.image.load('player.png').convert_alpha()
+player_rect = player.get_rect()
+player_speed = 10
 
 def create_enemy():
-    enemy = pygame.Surface((20, 20))
-    enemy.fill(RED)
+    # enemy = pygame.Surface((20, 20))
+    # enemy.fill(RED)
+    enemy = pygame.image.load('enemy.png').convert_alpha()
     enemy_rect = pygame.Rect(width, random.randint(0, heigth), *enemy.get_size())
     enemy_speed = random.randint(2, 5)
     return [enemy, enemy_rect, enemy_speed]
 
 def create_bonus():
-    bonus = pygame.Surface((20, 20))
-    bonus.fill(YELLOW)
+    # bonus = pygame.Surface((20, 20))
+    # bonus.fill(YELLOW)
+    bonus = pygame.image.load('bonus.png').convert_alpha()
     bonus_rect = pygame.Rect(random.randint(0, width), 0, *bonus.get_size())
     bonus_speed = random.randint(2, 5)
     return [bonus, bonus_rect, bonus_speed]  
+
+bg = pygame.transform.scale(pygame.image.load('background.png').convert() , screen)
+bgX = 0
+bgX2 = bg.get_width()
+bg_speed = 3
 
 CREATE_ENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(CREATE_ENEMY, 1500)
 CREATE_BONUS = pygame.USEREVENT + 2
 pygame.time.set_timer(CREATE_BONUS, 3000)
+
+scores = 0
 
 enemies = []
 bonuses = []
@@ -61,9 +73,25 @@ while is_working:
 
     pressed_keys = pygame.key.get_pressed()
 
-    main_surface.fill(BLACK)
+    # main_surface.fill(WHITE)
 
-    main_surface.blit(ball, ball_rect)
+    # main_surface.blit(bg, (0, 0))
+
+    bgX -= bg_speed
+    bgX2 -= bg_speed
+
+    if bgX < -bg.get_width():
+        bgX = bg.get_width()
+
+    if bgX2 < -bg.get_width():
+        bgX2 = bg.get_width()
+
+    main_surface.blit(bg, (bgX, 0))
+    main_surface.blit(bg, (bgX2, 0))
+
+    main_surface.blit(player, player_rect)
+
+    main_surface.blit(font.render(str(scores), True, BLACK), (width - 30, 0))
 
     for enemy in enemies:
         enemy[1] = enemy[1].move(-enemy[2], 0)
@@ -72,8 +100,8 @@ while is_working:
         if enemy[1].left < 0:
             enemies.pop(enemies.index(enemy))
 
-        if ball_rect.colliderect(enemy[1]):
-            enemies.pop(enemies.index(enemy))
+        if player_rect.colliderect(enemy[1]):
+            is_working = False
 
     for bonus in bonuses:
         bonus[1] = bonus[1].move(0, bonus[2])
@@ -82,19 +110,20 @@ while is_working:
         if bonus[1].bottom > heigth:
             bonuses.pop(bonuses.index(bonus))
 
-        if ball_rect.colliderect(bonus[1]):
+        if player_rect.colliderect(bonus[1]):
             bonuses.pop(bonuses.index(bonus))
+            scores += 1
 
-    if pressed_keys[K_DOWN] and not ball_rect.bottom >= heigth:
-        ball_rect = ball_rect.move(0, ball_speed)
+    if pressed_keys[K_DOWN] and not player_rect.bottom >= heigth:
+        player_rect = player_rect.move(0, player_speed)
 
-    if pressed_keys[K_UP] and not ball_rect.top < 0:
-        ball_rect = ball_rect.move(0, -ball_speed)
+    if pressed_keys[K_UP] and not player_rect.top < 0:
+        player_rect = player_rect.move(0, -player_speed)
 
-    if pressed_keys[K_RIGHT] and not ball_rect.right >= width:
-        ball_rect = ball_rect.move(ball_speed, 0)
+    if pressed_keys[K_RIGHT] and not player_rect.right >= width:
+        player_rect = player_rect.move(player_speed, 0)
 
-    if pressed_keys[K_LEFT] and not ball_rect.left < 0:
-        ball_rect = ball_rect.move(-ball_speed, 0)
+    if pressed_keys[K_LEFT] and not player_rect.left < 0:
+        player_rect = player_rect.move(-player_speed, 0)
 
     pygame.display.flip()
